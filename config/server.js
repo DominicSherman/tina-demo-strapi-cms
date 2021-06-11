7590369e8b2466400a13d9683a6c626d75c71ac5
Dominic Sherman <dominic.sherman98@gmail.com>
2021-06-11 14:27:53 -0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fc9b93449c34d1134636126170dc5066'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '059525de-918c-4102-b7bc-dfd5f48909bf'
+};
